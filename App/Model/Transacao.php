b1389bb978dc53ca4a029cffc72775349a9f58a0
Patrik Rufino <58515198+patrikrufino@users.noreply.github.com>
2021-05-31 19:10:26 -0300

    Update Transacao.php

--- App/Model/Transacao.php
+++ App/Model/Transacao.php
@@ -1,7 +1,7 @@
-c<?php
+<?php
 class Transacao
 {
-    private $idtransacao;
+    private $id_transacao;
     private $valor;
     private $data;
     private $tipo;
@@ -10,6 +10,15 @@ class Transacao
 
 }
 
+// Getters e Setter
+public function getIdTransacao()
+{
+    return $this->id_transacao;
+}
 
+public function setIdTransacao($id_transacao)
+{
+    $this->$id_transacao = $id_transacao;
+}
 
 ?>
