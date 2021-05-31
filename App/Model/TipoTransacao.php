79b166519565c83efb038a78f33d61cd4b1a2a82
Isabela Dias <isabeladias@MacBook-Air-de-Isabela.local>
2021-05-31 09:08:43 -0300

    Atualizacao Models

---
+++ App/Model/TipoTransacao.php
@@ -0,0 +1,12 @@
+<?php
+class TipoTransacao{
+    private $idtransacao;
+    private $tipo;
+
+}
+
+
+
+
+
+?>
---
+++ App/Model/Transacao.php
@@ -0,0 +1,15 @@
+c<?php
+class Transacao
+{
+    private $idtransacao;
+    private $valor;
+    private $data;
+    private $tipo;
+    private $id_usuario;
+    private $id_livro;
+
+}
+
+
+
+?>
